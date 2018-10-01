a88b70d82db078293a1ca06f2743824782627fb2
C00206724 <c00206724@itcarlow.ie>
2018-10-01 10:59:50 +0100

    Proxy Implimentation

---
+++ Proxy_Implimentation/Graphic.cpp
@@ -0,0 +1,18 @@
+#include "Graphic.h"
+
+
+
+#include <iostream>
+#include "Graphic.h"
+
+Graphic::Graphic() {
+
+}
+
+Graphic::~Graphic() {
+
+}
+
+void Graphic::Draw() {
+	std::cout << "Draw Graphic Object" << std::endl;
+}
