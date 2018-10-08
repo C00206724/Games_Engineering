0bc8e0b7c4ab0f49b0f4d2858762b5142595ebf7
C00206724 <c00206724@itcarlow.ie>
2018-10-08 19:22:36 +0100

    works like a charm

---
+++ CommandPattern/Main.cpp
@@ -0,0 +1,66 @@
+#include "InputHandler.h"
+#include <SDL.h>
+#include <stdio.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+int main(int argc, char* args[])
+{
+	SDL_Event event;
+	InputHandler handler = InputHandler();
+	int quit = 0;
+
+	//The window we'll be rendering to
+	SDL_Window* window = NULL;
+
+	//The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		//Create window
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			//Get window surface
+			screenSurface = SDL_GetWindowSurface(window);
+
+			//Fill the surface white
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+			//Update the surface
+			SDL_UpdateWindowSurface(window);
+
+			//2 second waiting
+			SDL_Delay(2000);
+		}
+	}
+
+	while (!quit)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			handler.handleInput(event);
+		}
+	}
+
+	//Destroy window
+	SDL_DestroyWindow(window);
+
+	//Quit SDL subsystems
+	SDL_Quit();
+
+	return 0;
+}
